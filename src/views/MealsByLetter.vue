f3acd280ce609e10503460d9a849b0a6990cbb99
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-10-14 18:38:23 +0400

    Improve styling for letters

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -2,12 +2,12 @@
   <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals by Letter</h1>
   </div>
-  <div class="flex justify-center gap-3 px-8">
+  <div class="flex flex-wrap justify-center gap-3 px-8 mb-6">
     <router-link
       :to="{ name: 'byLetter', params: { letter } }"
       v-for="letter of letters"
       :key="letter"
-      class="hover:text-orange-500 transition-colors"
+      class="w-2 h-2 flex items-center justify-center hover:text-orange-500 hover:scale-150 transition-all"
     >
       {{ letter }}
     </router-link>
